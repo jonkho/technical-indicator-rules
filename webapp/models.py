67faf7bf89d1228679c3ff0d27882a3c1f86b9a0
Jonathan Ho <kinshing@gmail.com>
2010-09-26 05:43:45 +0000

    new formula to calculate strategy performance correction

--- webapp/models.py
+++ webapp/models.py
@@ -470,7 +470,7 @@ class Account(object):
         buy_and_hold_value = self.hypothetical_buy_and_hold_value(price_history_data[0][4], price_history_data[-1][4])
         strategy_value = self.value(price_history_data[-1][4])
         
-        buy_and_hold_value_as_percent_of_strategy_value = buy_and_hold_value / strategy_value
+        buy_and_hold_value_as_percent_of_strategy_value = (buy_and_hold_value / strategy_value) * 100
         performance_delta = 100 - buy_and_hold_value_as_percent_of_strategy_value
         
         summary = {"buy_and_hold_value":'%.2f' % buy_and_hold_value, "strategy_value": '%.2f' % strategy_value, "performance_delta":'%.2f' % performance_delta, "trade_history":self.trade_history}
