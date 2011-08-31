55b9e519bfa96e82cbd8020f40e94913dd2318f9
jonkho <github@silverfan.otherinbox.com>
2011-08-31 15:17:07 -0500

    Edited settings.py via GitHub

--- settings.py
+++ settings.py
@@ -19,9 +19,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': './local.db',                      # Or path to database file if using sqlite3.
-        'USER': 'sparrowuser',                      # Not used with sqlite3.
-        'PASSWORD': 'aust1nsparr0wtr8de',                  # Not used with sqlite3.
-        'HOST': '173.203.90.149',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
